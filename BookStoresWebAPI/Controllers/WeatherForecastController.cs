98860ce5d189ca07077c6c443c250285acdcf2b5
Nadeera Wanniarachchi <67604931+Nadeera551@users.noreply.github.com>
2021-01-20 11:46:02 +0530

    Add files via upload

---
+++ BookStoresWebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using BookStoresWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoresWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+
+        [HttpGet]
+        public IEnumerable<Publisher> Get()
+        {
+            //CRUD
+            using(var _context = new BookStoresDBContext())
+            {
+                // Publisher publisher = new Publisher();
+                // publisher.PublisherName = "Harry Potter";
+
+                // _context.Publishers.Add(publisher);
+
+                // _context.SaveChanges();
+                Publisher publisher = _context.Publishers.FirstOrDefault();
+                // publisher.PublisherName = "Harry Potter1";
+
+                _context.Publishers.Remove(publisher);
+
+                _context.SaveChanges();
+
+                return _context.Publishers.ToList();
+
+            }
+            
+        }
+    }
+}
---
+++ BookStoresWebAPI/Models/BookStoresDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoresWebAPI.Models
+{
+    public class BookStoresDBContext : DbContext
+    {
+        public DbSet<Publisher>Publishers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Data Source=DESKTOP-3OE8PHC;Initial Catalog=BookStoresDB;Integrated Security=True");
+        }
+    }
+}
